7f867ea9e9e3610e4a44d49cfb27c8c42f5c8f56
Ranjith <ranjithkumarseekolu@gmail.com>
2024-01-24 19:10:46 +0530

    Fix prod

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,8 +1,8 @@
 console.log("NODE_ENV:", process.env.NODE_ENV);
 const apiUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://car-rental-backend-u6r1.onrender.com/"
-    : "http://localhost:3000/";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/"
+    : "https://car-rental-backend-u6r1.onrender.com/";
 
 console.log("apiUrl:", apiUrl);
 export default apiUrl;
