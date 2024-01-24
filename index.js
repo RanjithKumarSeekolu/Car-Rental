7a5b446bbaf4802de2d3b0ba2fd433a40bad575c
Ranjith <ranjithkumarseekolu@gmail.com>
2024-01-24 15:50:14 +0530

    Fix index.js

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { appRouter } from "./src/App";
 import { RouterProvider } from "react-router-dom";
-
-import Tracker from "@openreplay/tracker";
-
-const tracker = new Tracker({
-  projectKey: "0tpLjDvQF3XA3tAQAy7h",
-});
-tracker.start();
-window.tracker = tracker;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={appRouter} />);
